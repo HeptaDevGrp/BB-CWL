92079a0d767e4f46e80a5ea75f0974f7d7328888
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-07-28 15:00:16 +0800

    crawler completed

---
+++ Selenium/crawler_helper.py
@@ -0,0 +1,28 @@
+import time
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.chrome.options import Options
+
+
+def wait_for_load(driver):
+    elem = driver.find_element_by_tag_name('html')
+    count = 0
+    while True:
+        count += 1
+        if count > 20:
+            print('Timing out after 10 seconds and returning')
+            return
+        time.sleep(.5)
+        try:
+            elem == driver.find_element_by_tag_name('html')
+        except StaleElementReferenceException:
+            return
+
+
+def headless_mode_initialization():
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-extensions")
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--no-sandbox") # linux only
+    chrome_options.add_argument("--headless")
+    # chrome_options.headless = True # also works
+    return chrome_options
--- Selenium/main.py
+++ Selenium/main.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import configparser
+from crawler_helper import headless_mode_initialization
 from time import sleep
 from login import login
 
+# set up chrome
+chrome_options = headless_mode_initialization()
+driver = webdriver.Chrome(options=chrome_options)
+
 # open the Blackboard Website
-driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.get("https://learn.intl.zju.edu.cn/")
 
@@ -18,7 +20,16 @@ driver.implicitly_wait(10)
 agree_button = driver.find_element_by_xpath('/html/body/div[8]/div/div/div/div/div/div/div[2]/button')
 agree_button.click()
 
-# successfully logged in
+# click "announcements"
+driver.implicitly_wait(10)
+announcements_button = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div/div/div/div/div[1]/div[1]/div[2]/ul/li[1]/a')
+announcements_button.click()
+
+# retrieve the form
+print(driver.page_source)
+
+# wait for next command
 sleep(100)
+
 
 driver.quit()
