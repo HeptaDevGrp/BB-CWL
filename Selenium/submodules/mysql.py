72100aa65bb8b4b454232ace8130c24ed217814d
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-08-04 16:01:23 +0800

    completed the mysql scaffold

--- Selenium/submodules/mysql.py
+++ Selenium/submodules/mysql.py
@@ -1,5 +1,17 @@
+import MySQLdb
+
+
 def read_into_mysql(data):
+    db = MySQLdb.connect("localhost", "testuser", "test123", "TESTDB", charset='utf8')
+    cursor = db.cursor()
+
     for line in data.splitlines():
-        pass
+        # 使用execute方法执行SQL语句
+        cursor.execute("SELECT VERSION()")
+        # 使用 fetchone() 方法获取一条数据
+        data = cursor.fetchone()
+        print("Database version : %s " % data)
+
+    db.close()
 
     return
