c8df644e2f8dbc8ec56afda0e8c239bf7fa977b6
zsi2 <zsi2@illinois.edu>
2021-08-06 22:41:58 +0800

    1

---
+++ Code/Client/BB-CWL-Client/pages/tabbar/index.vue
@@ -0,0 +1,54 @@
+
+<template>
+	<view class="base">
+		<scroll-view scroll-y style="height: 1500rpx;width: 100%;" @scrolltolower="reachBottom">
+			<view class="back">
+		
+			</view>
+			<view class="announcementstatic">
+				<view class="announcementedge"></view>
+				<view class="announcementco1"></view>
+				<view class="announcementco2"></view>
+				<view class="announcementcotext">Announcements</view>
+			</view>
+			<view class="clockbutton">
+			
+			</view>
+			<view class="calendarbutton">
+
+			</view>
+			<view class="today">Today</view>
+			<view class="tomorrow">Tomorrow</view>
+			<view class="future">Future</view>
+			<view class="todaylogo"></view>
+			<view class="tomorrowlogo"></view>
+			<view class="futurelogo"></view>
+		
+		</scroll-view>	
+		<view class="bottom">
+			
+		</view>
+	</view>		
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+@import "/pages/tabbar/index.css";
+
+</style>
