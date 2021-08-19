1388e0386006e7e3375a27d1c97eb3166393714d
lyon <liyang.19@intl.zju.edu.cn>
2021-08-19 21:33:48 +0800

    multi-thread add

--- Code/Selenium/main.py
+++ Code/Selenium/main.py
@@ -1,14 +1,18 @@
 from bb_cwl import BB_CWL_retrieve_data
 import threading
-# assume there are 16 people
+
+# set the user's number
 user_number=2
 threading_array=[]
+
+# create the thread and make it as protect process
 if __name__=='__main__':
-    # create the thread and make it as protect process
     for i in range(user_number):
         son_thread=threading.Thread(target=BB_CWL_retrieve_data,args=('_'+str(i),))
         threading_array.append(son_thread)
         threading_array[i].start()
+    
     for son_thread in threading_array:
         son_thread.join()
+
 print("======end of the main programme=======")
