20061ea578c95a12a344cd8ad39aa48f662521f2
lyon <liyang.19@intl.zju.edu.cn>
2021-08-27 16:00:15 +0800

    thread-check version

--- Code/Selenium/main.py
+++ Code/Selenium/main.py
@@ -1,25 +1,34 @@
 from bb_cwl import BB_CWL_retrieve_data
 import threading
 import os
+import time
 
 # set the user's number
 user_number=1
 threading_array=[]
 linux_clear_process_cmd = "ps -ef|grep firefox | grep -v grep | awk '{print $2}' | xargs kill -9"
-# create the thread and store
+
+# create the thread and store these threads
 for i in range(user_number):
     son_thread=threading.Thread(target=BB_CWL_retrieve_data,args=('_'+str(i),))
     threading_array.append(son_thread)
 
 # run the multi-thread
-while(True):
-    if __name__=='__main__':
+if __name__=='__main__':
+    for i in range(user_number):
+        threading_array[i].start()
+    
+    # check and restart the threads if required.
+    while(True):
         for i in range(user_number):
-            threading_array[i].start()
-            
-        for son_thread in threading_array:
-            son_thread.join()
+            if not threading_array[i].is_alive():
+                print("======restart the thread_"+str(i)+"===========")
+                threading_array[i] = threading.Thread(target=BB_CWL_retrieve_data,args=('_'+str(i),))
+                threading_array[i].start()
+        
+        # check every half an hour
+        time.sleep(1800)
+    
     print("======end of the main programme=======")
-    # restart the programme and clear the firefox process not quitting
-    print("======restart the thread===========")
+    #clear the firefox process not quitting
     os.system(linux_clear_process_cmd)
