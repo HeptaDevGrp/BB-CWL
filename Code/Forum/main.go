979922aa826c5921edf2841a67881c1837791e8f
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-09-28 19:52:30 +0800

    added all the go files up

--- Code/Forum/main.go
+++ Code/Forum/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+    "fmt"
+    "log"
+    "net/http"
 )
 
+/*
+func main()  {
+    startWebServer()
+}
+
+// 通过指定端口启动 Web 服务器
+func startWebServer()  {
+    r := NewRouter() // 通过 router.go 中定义的路由器来分发请求
+
+    // 处理静态资源文件
+    assets := http.FileServer(http.Dir(ViperConfig.App.Static))
+    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", assets))
+
+    http.Handle("/", r)
+
+    log.Println("Starting HTTP service at " + ViperConfig.App.Address)
+    err := http.ListenAndServe(ViperConfig.App.Address, nil)
+
+    if err != nil {
+        log.Println("An error occured starting HTTP listener at " + ViperConfig.App.Address)
+        log.Println("Error: " + err.Error())
+    }
+}
+*/
+
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hello!")
+    })
+
+
+    fmt.Printf("Starting server at port 8080\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
