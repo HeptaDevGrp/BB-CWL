a3334879778a6b233a32e4fdeca5799c1ca055f1
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-09-28 19:05:37 +0800

    completed the models part

--- Code/Forum/models/db.go
+++ Code/Forum/models/db.go
@@ -13,5 +13,30 @@ var Db *sql.DB
 
 func init() {
 	var err error
+	Db, err = sql.Open("mysql", "root:root@/chitchat?charset=utf8&parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
 
+func createUUID() (uuid string/*return*/) {
+	u := new([16]byte)
+	_/*throw away*/, err := rand.Read(u[:])
+	if err != nil {
+		log.Fatalln("Cannot generate UUID", err)
+	}
+
+	// 0x40 is reserved variant from RFC 4122
+	u[8] = (u[8] | 0x40) & 0x7F
+	// Set the four most significant bits (bits 12 through 15) of the
+	// time_hi_and_version field to the 4-bit version number.
+	u[6] = (u[6] & 0xF) | (0x4 << 4)
+	uuid = fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
+	return uuid
+}
+
+func Encrypt(plaintext string/*argument*/) (encrypted string /*return*/) {
+	encrypted = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	return encrypted
 }
