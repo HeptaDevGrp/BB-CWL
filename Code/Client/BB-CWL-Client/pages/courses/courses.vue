23dcd0fab006d48888b60dd7f1743d020008e6b6
luckfort <huangjingyuan5@163.com>
2022-02-13 23:20:08 +0800

    course basic is finished.

--- Code/Client/BB-CWL-Client/pages/courses/courses.vue
+++ Code/Client/BB-CWL-Client/pages/courses/courses.vue
@@ -1,8 +1,36 @@
 <template>
+	<scroll-view scroll-y style="height: 1600rpx;width: 100%;" @scrolltolower="reachBottom">
+		<meta charset="utf-8">
+		<view class="courses">
+			<view class="courses_str">Courses</view>
+			<view class="title_bar"></view>
+			<view class="layer_3"></view>
+			<view class="组_1">
+				<view class="courses_str">Courses</view>
+				<view class="矩形_4"></view>
+			</view>
+			<view class="组_2 ">
+				<view class="矩形_9"></view>
+				<view class="矩形_91"></view>
+				<view class="矩形_92"></view>
+				<view class="图层_12"></view>
+			</view>
+			<view class="task栏">
+				<view class="矩形_8"></view>
+				<view class="矩形_81"></view>
+				<view class="矩形_82"></view>
+				<view class="矩形_83"></view>
+				<view class="矩形_84"></view>
+				<view class="矩形_85"></view>
+				<view class="矩形_86"></view>
+			</view>
+		</view>
+	</scroll-view>
 </template>
 
 <script>
 </script>
 
 <style>
+@import "/pages/courses/course1.css";
 </style>
