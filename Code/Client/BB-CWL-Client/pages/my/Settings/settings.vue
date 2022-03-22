043ebda89a560105907d144db2a23d142ea9e8ab
luckfort <huangjingyuan5@163.com>
2022-03-22 23:14:07 +0800

    Add my, settings

---
+++ Code/Client/BB-CWL-Client/pages/my/Settings/settings.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="settings">
+		<view class="Back" @click="goBack()"></view>
+		<view class="underline"></view>
+		<view class="security"></view>
+		<view class="profile"></view>
+		<view class="Account">Account</view>
+		<view class="Setting">Setting</view>
+		<view class="USER1234">USER1234</view>
+		<view class="profile_photo"></view>
+		<view class="Logout">Logout</view>
+		<view class="box"></view>
+		<view class="top"></view>
+		<view class="middle"></view>
+		<view class="top2"></view>
+		<view class="_1"></view>
+		<view class="_2"></view>
+		<view class="_3"></view>
+		<view class="bottom"></view>
+		<view class="background"></view>
+	</view>
+</template>
+<script>
+	import json from '../../../../Node/client_helper/import_data.js';
+	export default {
+		methods: {
+			goBack(){
+				uni.navigateBack({
+					delta: 1
+				})
+			}
+		}
+	}
+</script>
+<style>
+	@import "/pages/my/Settings/settings.css";
+</style>
--- Code/Client/BB-CWL-Client/pages/my/my.vue
+++ Code/Client/BB-CWL-Client/pages/my/my.vue
@@ -1,8 +1,34 @@
 <template>
+	<view class="My">
+		<view class="top"></view>
+		<view class="top2"></view>
+		<view class="middle">
+			<view class="profile_photo"></view>
+			<view class="USER1234">USER1234</view>
+			<view class="useID123">useID123</view>
+		</view>
+		<view class="click">
+			<view class="Message"></view>
+			<view class="Setting" @click="my2set()"></view>
+		</view>
+		<view class="User_Guide"></view>
+	</view>
 </template>
 
 <script>
+	import json from '../../../../Node/client_helper/import_data.js';
+	export default {
+		methods: {
+			my2set(){
+				console.log('1');
+				uni.navigateTo({
+					url: './Settings/settings',
+				})
+			}
+		}
+	}
 </script>
 
 <style>
+	@import "/pages/my/my.css";
 </style>
